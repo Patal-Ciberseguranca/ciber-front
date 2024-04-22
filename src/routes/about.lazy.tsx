604ffe545043e1eec75bd897eb194a9cf716d1a2
Eduardo Abrantes <81658559+PatalJunior@users.noreply.github.com>
2024-04-22 15:20:13 +0100

    Test

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -4,6 +4,6 @@ export const Route = createLazyFileRoute('/about')({
   component: About,
 });
 
-      function About() {
-        return <div className="p-2">Hello from About!</div>;
-      }
+function About() {
+  return <div className="p-2">Hello from About!</div>;
+}

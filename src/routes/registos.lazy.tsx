c83c8515101e72ad147eb725a6be29e14939a31d
JeyCraftPT <joaoduartelopi@gmail.com>
2024-05-15 15:43:00 +0100

    front
    front

--- src/routes/registos.lazy.tsx
+++ src/routes/registos.lazy.tsx
@@ -1,9 +1,11 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
-import { BsFillCameraVideoFill } from "react-icons/bs";
-import { IoMdPersonAdd } from "react-icons/io";
-import { IoIosMore } from "react-icons/io";
-import { GoPaperclip } from "react-icons/go";
-import { CiImageOn } from "react-icons/ci";
+/* import { BsFillCameraVideoFill } from 'react-icons/bs';
+import { IoMdPersonAdd } from 'react-icons/io'; 
+import { IoIosMore } from 'react-icons/io';
+import { GoPaperclip } from 'react-icons/go';
+import { CiImageOn } from 'react-icons/ci'; */
+import { IoIosSave } from "react-icons/io";
+
 
 export const Route = createLazyFileRoute('/registos')({
   component: Registos,
@@ -11,15 +13,16 @@ export const Route = createLazyFileRoute('/registos')({
 
 function Registos() {
   return (
-    <div className="h-screen bg-background text-white flex justify-center items-center ">
+    <div className="h-[92%] bg-background text-white flex justify-center items-center ">
       {/* Container */}
-      <div className="border-2 border-white rounded-xl w-10/12 h-5/6 flex truncate">
+      <div className="border-2 border-white rounded-xl w-10/12 h-5/6 flex truncate h-100%">
         {/* SideBar */}
         <div className="flex-1 border-rig bg-gray-700">
           {/* Barra de Navegação (Nome da APP e Nome do Utilizador) */}
-          <div className="flex items-center bg-gray-800 h-18 p-3 justify-between">
+          <div className="flex items-center bg-gray-800 h-20 p-3 justify-between">
             {/* Logo */}
             <span className="font-bold">CANTTOUCHME</span>
+
 
             {/* User */}
             <div>
@@ -37,14 +40,7 @@ function Registos() {
           {/* Barra de Pesquisa */}
           <div className="border-b-2 border-style: solid">
             {/* Pesquisar */}
-            <div className="p-3">
-              {/* searchform */}
-              <input
-                type="text"
-                placeholder="Procure um Utilizador"
-                className="border-black text-black font-bold outline-none"
-              />
-            </div>
+          
 
             {/* Outras Contas de Utilizadores 1 */}
             <div className="p-3 flex items-center gap-2.5 cursor-pointer hover:bg-gray-800">
@@ -77,52 +73,59 @@ function Registos() {
         {/* Chat */}
         <div className="flex-1 bg-gray-500">
           {/* Barra de Informação do Chat */}
-          <div className="h-12 bg-gray-700 flex items-center justify-between p-3 text-gray-200">
+          <div className="h-20 bg-gray-700 flex items-center justify-between p-3 text-gray-200">
             {/* chatInfo */}
             <span>João Carneiro</span>
             <div className="flex gap-3">
               {/* chatIcons */}
-              <BsFillCameraVideoFill className="cursor-pointer h-6" />
-              <IoMdPersonAdd className="cursor-pointer h-6" />
-              <IoIosMore className="cursor-pointer h-6" />
+              {/* <BsFillCameraVideoFill className="cursor-pointer h-6" />
+              <IoMdPersonAdd className="cursor-pointer h-6" /> 
+              <IoIosMore className="cursor-pointer h-6" /> */}
+              <IoIosSave className="cursor-pointer h-10 w-10" />
+
             </div>
           </div>
 
+          <textarea name="" id=""className='w-[80%] h-[75%] p-3 text-black rounded-md ml-[10%] mt-[6%]'>
+            
+          </textarea>
+
           {/* Mensagens */}
-          <div className="bg-gray-250 p-3 text-black h-full mb-[-95px] overflow-scroll">
-            {/* Mensagem Nº1 */}
+
+          {/* <div className= "bg-gray-250 p-3 text-black h-full overflow-scroll" >
+            
             <div className="flex gap-5 mb-5">
-              {/* Informação da Mensagem (Tempo da Mensagem e Imagem do Utilizador) */}
+              
               <div className="flex flex-col text-gray-200 font-light">
                 <img src="" className="w-10 h-10 rounded-xl object-cover" />
                 <span>Agora Mesmo</span>
               </div>
 
-              {/* Contéudo da Mensagem Enviada */}
+              
               <div className="max-w-[4/5] flex flex-col gap-3">
                 <p className="bg-white p-3 rounded">teste mensagem</p>
                 <img src="" className="" />
               </div>
             </div>
 
-            {/* Mensagem Nº2 */}
+            
             <div className="flex gap-5 mb-5">
-              {/* Informação da Mensagem (Tempo da Mensagem e Imagem do Utilizador) */}
+              
               <div className="flex flex-col text-gray-200 font-light">
                 <img src="" className="w-10 h-10 rounded-xl object-cover" />
                 <span>Agora Mesmo</span>
               </div>
 
-              {/* Contéudo da Mensagem Enviada */}
+              
               <div className="max-w-[4/5] flex flex-col gap-3">
                 <p className="bg-white p-3 rounded">teste mensagem 2</p>
                 <img src="" className="" />
               </div>
             </div>
-          </div>
+          </div> */}
 
           {/* Input de Texto e Anexo de Imagens */}
-          <div className="h-12 bg-white p-3 text-black flex items-center justify-between">
+          {/* <div className="h-12 bg-white p-3 text-black flex items-center justify-between">
             <input
               type="text"
               placeholder="Escreva uma mensagem de texto"
@@ -139,9 +142,10 @@ function Registos() {
                 Send
               </button>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
+    
   );
 }
